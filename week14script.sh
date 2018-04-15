66168a83703db2fabbc179967f23f33df6f0f592
kaoxiong <35321153+kaoxiong@users.noreply.github.com>
2018-04-15 16:53:01 -0500

    Create week14script.sh

---
+++ week14script.sh
@@ -0,0 +1,12 @@
+clear
+echo "Showing the device configuration on your machine."
+sleep 2
+echo " "
+xinput --list
+echo " "
+sleep 2
+Echo "We will Explore X Clients."
+sleep 2
+echo " "
+xwininfo
+echo " "
